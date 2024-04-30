858036367a04c04f717c2d5658777a6412730ec9
Jacob-Canyon <145243883+Jacob-Canyon@users.noreply.github.com>
2024-04-30 14:29:06 -0700

    aduit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,18 @@
 document.querySelector('#year').innerText = new Date().getFullYear();
 
 document.querySelector('#lastModified').innerHTML = "Last Modified: " + document.lastModified;
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        modeButton.textContent = "🔆";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+});
