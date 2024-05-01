939b0c853a8e0db4b64aeed2f8e555ce3622fea0
Jacob-Canyon <145243883+Jacob-Canyon@users.noreply.github.com>
2024-05-01 12:56:38 -0700

    for review

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,2 +1,11 @@
 
 document.querySelector('#lastModified').innerHTML = "Last Modified: " + document.lastModified;
+
+const hamButton = document.querySelector('#hamButton');
+
+const navElement = document.querySelector('.menuLinks');
+
+hamButton.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+});
+
--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,4 +1,10 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation')
 
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open')
+})
 
 document.querySelector('#year').innerText = new Date().getFullYear();
 
